bb7bd803e102745ceb0d3d896588d43df9644af4
liming <18829571775@163.com>
2018-09-12 10:10:50 +0800

    二叉树遍历求后序

---
+++ src/com/lego/utils/string/Main.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司 FileName: Main Author:   liming Date:     2018/9/12 9:55
+ * Description: text History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.lego.utils.string;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈text〉
+ *
+ * @author liming
+ * @create 2018/9/12
+ * @since 1.0.0
+ */
+public class Main {
+public static void main(String []args){
+  int x=1;
+  do{
+    x++;
+  }while(x<=4);
+  System.out.println(x);
+}
+}
